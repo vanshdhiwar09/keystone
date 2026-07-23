807577783f2dc530488bb045a174f82f632bebfa
Vansh Dhiwar <vansh.dhiwar985@gmail.com>
2026-07-23 17:27:51 +0530

    chore: prepare repository for production release

--- test_and_build.bat
+++
@@ -1,8 +0,0 @@
-cargo test -p escrow --color=never > output.log 2>&1
-echo ============================== >> output.log
-cargo test -p fee-router --color=never >> output.log 2>&1
-echo ============================== >> output.log
-cargo test -p payout --color=never >> output.log 2>&1
-stellar contract build
-echo ============================== >> output.log
-powershell -Command "Get-Item target\wasm32v1-none\release\*.wasm | Select-Object Name, Length" >> output.log
